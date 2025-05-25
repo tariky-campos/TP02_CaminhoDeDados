92d76d186b793f77986ac00f68c3bad7c7dc7a86
tariky-campos <tariky.campos@ufv.br>
2025-05-25 14:05:08 -0300

    vamoo

--- Inputs/instruction1.asm
+++ Inputs/instruction1.asm
@@ -1,6 +1,5 @@
-lb x1, 0(x2)       # Carrega byte da memória no endereço x2 + 0 para o registrador x1
-ori x3, x1, 5      # x3 = x1 | 5
-and x4, x3, x5     # x4 = x3 & x5
-sll x6, x4, x7     # x6 = x4 << (x7 & 0x1F)
-add x8, x6, x9     # x8 = x6 + x9
-sb x8, 4(x2)       # Armazena o byte de x8 na memória no endereço x2 + 4
+addi x2, x0, 7
+addi x3, x0, 5
+beq x0,x0, loop
+loop:
+addi x5,x0,12
