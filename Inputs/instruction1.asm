70933a210c712516d781b295838da8a9cf5c4af4
tariky-campos <tariky.campos@ufv.br>
2025-05-24 21:43:14 -0300

    add instructions grp

--- Inputs/instruction1.asm
+++ Inputs/instruction1.asm
@@ -1,15 +1,6 @@
-addi x2, x0, 7
-sw x2, 4(x0)
-lw x1, 4(x0)
-add x2, x1, x0
-add x1, x1, x2
-add x1, x1, x2
-sub x1, x1, x2
-sub x1, x1, x2
-beq x1, x2, SAIDA
-add x1, x1, x1
-sw x1, 0(x0)
-SAIDA:
-and x1, x1, x2
-or x1, x1, x0
-sw x1, 0(x0)
+lb x1, 0(x2)       # Carrega byte da memória no endereço x2 + 0 para o registrador x1
+ori x3, x1, 5      # x3 = x1 | 5
+and x4, x3, x5     # x4 = x3 & x5
+sll x6, x4, x7     # x6 = x4 << (x7 & 0x1F)
+add x8, x6, x9     # x8 = x6 + x9
+sb x8, 4(x2)       # Armazena o byte de x8 na memória no endereço x2 + 4
